d51591ae86e08c7aea909355bac9bec716bebedd
mikessut <mikesutton@gmail.com>
2025-01-06 16:38:59 -0700

    cleanup

--- siriusxm.py
+++ siriusxm.py
@@ -4,6 +4,13 @@ https://xmplaylist.com/api/documentation
 import requests
 
 
-def coffeehouse():
-    r = requests.get('https://xmplaylist.com/api/station/thecoffeehouse')
+def get_channel(channel: str):
+    """
+    Some known channels:
+        thecoffeehouse
+        lifewithjohnmayer
+        theblend
+        thebridge
+    """
+    r = requests.get(f'https://xmplaylist.com/api/station/{channel}')
     return r.json()
--- spotify.py
+++ spotify.py
@@ -5,12 +5,33 @@ import os
 import siriusxm
 
 
-def add_coffeehouse_to_queue(sp: spotipy.Spotify):
-    ch = siriusxm.coffeehouse()
+def add_to_queue(sp: spotipy.Spotify, channel: str):
+    ch = siriusxm.get_channel(channel)
 
     for song in ch:
-        print(f"Adding: {song['track']['name']} by: {song['track']['artists']}")
-        sp.add_to_queue(song['spotify']['spotify_id'])
+        if 'spotify' in song.keys():
+            print(f"Adding: {song['track']['name']} by: {song['track']['artists']}")
+            sp.add_to_queue(song['spotify']['spotify_id'])
+        else:
+            print(f"No spotify data for: {song['track']['name']} by: {song['track']['artists']}")
+
+
+def refresh():
+    token_info = sp_oauth.get_cached_token()
+
+    if not token_info:
+        auth_url = sp_oauth.get_authorize_url()
+        print(auth_url)
+        response = input('Paste the above link into your browser, then paste the redirect url here: ')
+
+        code = sp_oauth.parse_response_code(response)
+        token_info = sp_oauth.get_access_token(code)
+
+    token = token_info['access_token']
+
+    sp = spotipy.Spotify(auth=token)
+
+    return sp
 
 
 if __name__ == '__main__':
@@ -32,18 +53,5 @@ if __name__ == '__main__':
                                 client_secret=client_secret,
                                 redirect_uri=redirect_uri,
                                 scope=scope)
-    token_info = sp_oauth.get_cached_token()
-
-    if not token_info:
-        auth_url = sp_oauth.get_authorize_url()
-        print(auth_url)
-        response = input('Paste the above link into your browser, then paste the redirect url here: ')
-
-        code = sp_oauth.parse_response_code(response)
-        token_info = sp_oauth.get_access_token(code)
-
-        token = token_info['access_token']
-
-
-    if token:
-        sp = spotipy.Spotify(auth=token)
+    
+    sp = refresh()
